490ab08995bd63556cb0874dc2666efcecc953c0
hazemgabr232 <hazemgabr232@gmail.com>
2019-08-10 02:52:19 +0200

    add timer handler (PIT)

---
+++ cpu/timer.c
@@ -0,0 +1,31 @@
+#include "timer.h"
+#include "../drivers/screen.h"
+#include "../kernel/util.h"
+#include "isr.h"
+
+u32 tick = 0;
+
+static void timer_callback(registers_t regs) {
+    tick++;
+    kprint("Tick: ");
+    
+    char tick_ascii[256];
+    int_to_ascii(tick, tick_ascii);
+    kprint(tick_ascii);
+    kprint("\n");
+}
+
+void init_timer(u32 freq) {
+        add_interrupt_handler(IRQ0, timer_callback);
+
+    /* Get the PIT value: hardware clock at 1193180 Hz
+     * freq is more than(at least) 65535 that makes timer to tick every 54.9254 ms */
+    u32 divisor = 1193180 / freq;
+    u8 low  = (u8)(divisor & 0xFF);
+    u8 high = (u8)( (divisor >> 8) & 0xFF);
+    /* Send the command */
+    port_byte_out(0x43, 0x36); /* Command port */
+    port_byte_out(0x40, low);
+    port_byte_out(0x40, high);
+}
+
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,14 +1,11 @@
 #include "../drivers/screen.h"
-#include "util.h"
 #include "../cpu/isr.h"
-#include "../cpu/idt.h"
+#include "../cpu/timer.h"
 
 void main() {
     isr_install();
     /* Test the interrupts */
-    __asm__ __volatile__("int $0");
-    __asm__ __volatile__("int $1");
-    __asm__ __volatile__("int $2");
-    __asm__ __volatile__("int $3");
-    __asm__ __volatile__("int $4");
+
+    asm volatile("sti");
+    init_timer(50);
 }
